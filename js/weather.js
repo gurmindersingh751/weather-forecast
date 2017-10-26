47e972b062def3acc3beeffbec0ce0f2e4c499e8
Nancy Sharma <32308663+nancy43@users.noreply.github.com>
2017-10-26 12:31:05 -0400

    Add files via upload

---
+++ js/weather.js
@@ -0,0 +1,87 @@
+// Get a free key at http://openweathermap.org/. Replace the "Your_Key_Here" string with that key.
+var OpenWeatherAppKey = "b3cfb44eeec2986f589369724a654efc";
+
+function getWeatherWithZipCode() {
+
+    var zipcode = $('#zip-code-input').val();
+
+    var queryString =
+        'http://api.openweathermap.org/data/2.5/weather?zip='
+         + zipcode + ',us&appid=' + OpenWeatherAppKey + '&units=metric';
+
+    $.getJSON(queryString, function (results) {
+
+        showWeatherData(results);
+
+    }).fail(function (jqXHR) {
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. " + jqXHR.statusText);
+    });
+
+    return false;
+}
+
+function showWeatherData(results) {
+
+    if (results.weather.length) {
+
+        $('#error-msg').hide();
+        $('#weather-data').show();
+
+        $('#title').text(results.name);
+        $('#desc').text(results.weather[0].description);
+        $('#temperature').text(results.main.temp);
+         $('#temp').text(results.main.temp_min);
+        $('#pressure').text(results.main.pressure);
+        $('#wind').text(results.wind.speed);
+        $('#humidity').text(results.main.humidity);
+       
+        var sunriseDate = new Date(results.sys.sunrise * 1000);
+        $('#sunrise').text(sunriseDate.toLocaleTimeString());
+
+        var sunsetDate = new Date(results.sys.sunset * 1000);
+        $('#sunset').text(sunsetDate.toLocaleTimeString());
+
+    } else {
+        $('#weather-data').hide();
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. ");
+    }
+}
+
+function getWeatherWithGeoLocation() {
+
+    navigator.geolocation.getCurrentPosition(onGetLocationSuccess, onGetLocationError,
+      { enableHighAccuracy: true });
+
+    $('#error-msg').show();
+    $('#error-msg').text('Determining your current location ...');
+
+    $('#get-weather-btn').prop('disabled', true);
+}
+function onGetLocationSuccess(position) {
+
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    var queryString =
+      'http://api.openweathermap.org/data/2.5/weather?lat='
+      + latitude + '&lon=' + longitude + '&appid=' + OpenWeatherAppKey + '&units=metric';
+
+    $('#get-weather-btn').prop('disabled', false);
+
+    $.getJSON(queryString, function (results) {
+
+        showWeatherData(results);
+
+    }).fail(function (jqXHR) {
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. " + jqXHR.statusText);
+    });
+
+}
+function onGetLocationError(error) {
+
+    $('#error-msg').text('Error getting location');
+    $('#get-weather-btn').prop('disabled', false);
+}
